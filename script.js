29ee02cc8e22547152f08e58f7d9dd724b3b70a4
AdrianoGC24 <adrianogonzalezcera002@gmail.com>
2026-04-21 18:02:30 -0500

    feat: agregué validaciones, falta mostrar resultado

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-const pantalla = document.querySelector('.pantalla');
 const botones = document.querySelectorAll('.btn');
+const panelNumero1 = document.querySelector('#numero1');
+const panelOperacion = document.querySelector('#operacion');
+const panelNumero2 = document.querySelector('#numero2');
+const panelResultado = document.querySelector('#resultado');
 
+let numero1 = ''
+let numero2 = ''
+let operacion = ''
+
+panelNumero1.textContent = numero1;
+panelOperacion.textContent = operacion;
+panelNumero2.textContent = numero2;
+
+function actualizarPantalla () {
+    panelNumero1.textContent = numero1;
+    panelOperacion.textContent = operacion;
+    panelNumero2.textContent = numero2;
+    panelResultado.textContent = '';
+}
 
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
 
-        const botonapretado = boton.textContent;
+        const botonseleccionado = boton.textContent;
 
         if(boton.id === 'c') {
-            pantalla.textContent = '0';
+            numero1 = ''
+            numero2 = ''
+            operacion = ''
+            actualizarPantalla();
             return;
         }
 
-        if (boton.id === 'borrar') {
-            if (pantalla.textContent.length === 1 || pantalla.textContent === 'ERROR') {
-                pantalla.textContent ='0';
+        if (!isNaN(botonseleccionado)) {
+
+            if (operacion === '') {
+                numero1 += botonseleccionado;
             } else {
-                pantalla.textContent=pantalla.textContent.slice(0, -1);
+                numero2 += botonseleccionado;
             }
+
+            actualizarPantalla();
+            return;
+            }
+
+        if (boton.id === 'borrar'){
+            if (numero2 !== '') {
+                numero2 = numero2.slice(0, -1)
+            } else if (operacion !== ''){
+                operacion = ''
+            } else {
+                numero1 = numero1.slice(0,-1)
+            }
+
+            actualizarPantalla();
             return;
         }
+        
+
+        if (botonseleccionado === '+' || botonseleccionado === '-' || botonseleccionado === '*' || botonseleccionado === '/') {
+            if (numero1 === '') {
+                return;
+            }
+            operacion = botonseleccionado
+            actualizarPantalla()
+            return;
+        }
+
 
         if (boton.id === 'igual') {
-            try {
-                pantalla.textContent = eval(pantalla.textContent);
-            } 
-            catch {
-                pantalla.textContent = 'ERROR';
-            }
-            return;
-        }
-
-        if (pantalla.textContent === '0' || pantalla.textContent === 'ERROR') {
-            pantalla.textContent = botonapretado;
-        } else {
-            pantalla.textContent += botonapretado;
+            
         }
     })
 })
